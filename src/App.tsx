a3e3ccabd940e16cf459f18e616092d42d97957b
TajAlDenAjek <taj.darkn10@gmail.com>
2024-04-02 07:23:14 +0300

    dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import LayoutContainer from "./Layout/Layout"
-
+import { useState } from "react";
+import { ConfigProvider,theme} from "antd";
 const App = () => {
+  const { defaultAlgorithm, darkAlgorithm } = theme;
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
   return (
     <>
-      <LayoutContainer/>
+      <ConfigProvider
+        theme={{
+          algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+        }}>
+        <LayoutContainer />
+      </ConfigProvider>
     </>
   )
 }
