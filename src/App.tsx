f05101bb1cdf2e20d65943c5de9581b4da3f034a
TajAlDenAjek <taj.darkn10@gmail.com>
2024-04-25 07:53:53 +0300

    redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import LayoutContainer from "./Layout/Layout"
 import { useState } from "react";
-import { ConfigProvider,theme} from "antd";
+import { ConfigProvider, theme } from "antd";
+import { store } from "./app/store";
+import { Provider } from 'react-redux'
+
 const App = () => {
   const { defaultAlgorithm, darkAlgorithm } = theme;
   const [isDarkMode, setIsDarkMode] = useState(false);
-
   return (
     <>
-      <ConfigProvider
-        theme={{
-          algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
-        }}>
-        <LayoutContainer />
-      </ConfigProvider>
+      <Provider store={store}>
+        <ConfigProvider
+          theme={{
+            algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+          }}>
+          <LayoutContainer />
+        </ConfigProvider>
+      </Provider>
     </>
   )
 }
