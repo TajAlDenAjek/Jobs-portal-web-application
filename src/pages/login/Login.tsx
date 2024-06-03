d35904950823a08389160a7f269f533c8df1472d
TajAlDenAjek <taj.darkn10@gmail.com>
2024-06-03 07:28:39 +0300

    Login page styles

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -9,28 +9,28 @@ type LoginFieldType = {
   remember?: string
 }
 
-const onFinish: FormProps<LoginFieldType>['onFinish'] = (values) => {
-  console.log('values', values)
-}
-const onFinishFailed: FormProps<LoginFieldType>['onFinishFailed'] = (error) => {
-  console.log('error', error)
-}
 
 const Login = () => {
-
+  
+  const onFinish: FormProps<LoginFieldType>['onFinish'] = (values) => {
+    console.log('values', values)
+  }
+  const onFinishFailed: FormProps<LoginFieldType>['onFinishFailed'] = (error) => {
+    console.log('error', error)
+  }
   return (
     <div className='login-page'>
-      <Flex justify='center' align='center' style={{ height: '100vh' }}>
+      <Flex justify='center' align='center' style={{ height: '100vh' ,width:'100vw'}}>
         <Card className='login-card' >
           <h1 className='login-title'>Login Page</h1>
           <Form
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            style={{ maxWidth: 200 }}
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
             initialValues={{ remember: true }}
             onFinishFailed={onFinishFailed}
             autoComplete='off'
             onFinish={onFinish}
+            
           >
             <Form.Item<LoginFieldType> name="email" label="Email"
               rules={[{ required: true, message: 'Please Enter your email' }]}
@@ -42,7 +42,7 @@ const Login = () => {
             >
               <Input.Password />
             </Form.Item>
-            <Form.Item<LoginFieldType> name="remember" valuePropName='checked' wrapperCol={{ offset: 8, span: 16 }}>
+            <Form.Item<LoginFieldType> name="remember" valuePropName='checked' wrapperCol={{ offset: 0, span: 16 }}>
               <Checkbox>Remember me</Checkbox>
             </Form.Item>
             <Form.Item<LoginFieldType> valuePropName='checked' wrapperCol={{ offset: 8, span: 16 }}>
