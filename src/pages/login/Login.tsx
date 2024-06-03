118af9d06da87741b700290874fc5f3f78178413
TajAlDenAjek <taj.darkn10@gmail.com>
2024-06-03 07:12:33 +0300

    Login page

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,39 +1,53 @@
 import React, { useState } from 'react'
-import { Flex, Card, Form, Input, Button } from 'antd'
+import type { FormProps } from 'antd'
+import { Flex, Card, Form, Input, Button, Checkbox } from 'antd'
 import './style.css'
 
+type LoginFieldType = {
+  email?: string,
+  password?: string,
+  remember?: string
+}
+
+const onFinish: FormProps<LoginFieldType>['onFinish'] = (values) => {
+  console.log('values', values)
+}
+const onFinishFailed: FormProps<LoginFieldType>['onFinishFailed'] = (error) => {
+  console.log('error', error)
+}
 
 const Login = () => {
-  const [random, setRandom] = useState('')
 
   return (
     <div className='login-page'>
-
       <Flex justify='center' align='center' style={{ height: '100vh' }}>
         <Card className='login-card' >
           <h1 className='login-title'>Login Page</h1>
           <Form
-            onFinish={(values: any) => {
-              console.log(values)
-              console.log('hii')
-              setRandom('hi')
-            }}
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 200 }}
+            initialValues={{ remember: true }}
+            onFinishFailed={onFinishFailed}
+            autoComplete='off'
+            onFinish={onFinish}
           >
-            <Form.Item name={"email"} children={
-
-              <>
-                Email
-                <Input type='email' />
-              </>
-            } />
-            <Form.Item name={"password"} children={
-
-              <>
-                Password
-                <Input.Password />
-              </>
-            } />
-            <Button type='primary' formMethod='submit'>Sign in</Button>
+            <Form.Item<LoginFieldType> name="email" label="Email"
+              rules={[{ required: true, message: 'Please Enter your email' }]}
+            >
+              <Input type='email' />
+            </Form.Item>
+            <Form.Item<LoginFieldType> name="password" label="Password"
+              rules={[{ required: true, message: 'Please Enter your password' }]}
+            >
+              <Input.Password />
+            </Form.Item>
+            <Form.Item<LoginFieldType> name="remember" valuePropName='checked' wrapperCol={{ offset: 8, span: 16 }}>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+            <Form.Item<LoginFieldType> valuePropName='checked' wrapperCol={{ offset: 8, span: 16 }}>
+              <Button type='primary' htmlType='submit'>Sign in</Button>
+            </Form.Item>
           </Form>
         </Card>
       </Flex>
