75f32dfd1d16264cef42ae07ad26fb1b442f7be6
TajAlDenAjek <taj.darkn10@gmail.com>
2024-06-21 19:53:49 +0300

    search candidates

--- src/pages/findCandidates/FindCandidates.tsx
+++ src/pages/findCandidates/FindCandidates.tsx
@@ -6,25 +6,39 @@ import {Spin,Empty} from 'antd'
 import Candidate from './Candidate'
 import './styles.scss'
 const FindCandidates = () => {
+    const [searchValue, setSearchValue] = useState('')
     const {
         data,
+        currentData,
         isLoading,
         isSuccess,
         isError,
         error
     } = useGetProfilesQuery({})
+    const [filteredCandidates,setFilteredCandidates]=useState(currentData?.users)
+    useEffect(() => {
+        if (searchValue) {
+            setFilteredCandidates(currentData?.users?.filter((user: any) => {
+                return (user?.firstName?.toLowerCase()+' '+user?.lastName?.toLowerCase()).includes(searchValue.toLowerCase()) ||
+                user?.Specialization?.toLowerCase().includes(searchValue.toLowerCase())
+            }))
+        } else {
+            setFilteredCandidates(currentData?.users)
+        }
+    }, [searchValue])
+
 
     let content = <Empty/>
     if (isLoading) {
         content=<Spin/>
     } else if (isSuccess) {
         content = (
-            data?.users?.map((profile: any, index: any) => {
+            filteredCandidates?.map((profile: any, index: any) => {
                 return <Candidate key={index} profile={profile} />
             })
         )
-        if (data?.users?.length === 0) {
-            content = <Empty/>
+        if (!filteredCandidates || filteredCandidates.length === 0) {
+            content = <Empty />
         }
     } else if (isError) {
         content=<>{error}</>
@@ -33,7 +47,11 @@ const FindCandidates = () => {
 
     return (
         <div className='find-candidates-page'>
-            <Input.Search />
+            <Input.Search
+                placeholder="Search candidates by name or specialization"
+                value={searchValue}
+                onChange={(e) => setSearchValue(e.target.value)}
+            />
             {content}
         </div>
     )
