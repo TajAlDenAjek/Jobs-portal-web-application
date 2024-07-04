e7a96cc4316e734beb09924bff50b680bc645e8b
TajAlDenAjek <taj.darkn10@gmail.com>
2024-07-05 00:38:30 +0300

    user table

--- src/componenets/adminTable/AdminTable.tsx
+++ src/componenets/adminTable/AdminTable.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Space, Table, Tag,Typography } from 'antd';
 import type { TableProps } from 'antd';
-
+import './style.scss'
 
 interface AdminTableProps {
     tableTitle?: String
     data?: any
-    columns?: any
+    columns?: any,
+    isLoading?: boolean
 }
 const AdminTable:React.FC<AdminTableProps> = ({
     tableTitle,
     data,
-    columns
+    columns,
+    isLoading=false
 }) => {
 
     return (
-        <Table columns={columns} dataSource={data} />
+        <div className="table-container">
+            <h3>{tableTitle}</h3>
+            <Table columns={columns} dataSource={data} loading={isLoading} />
+        </div>
     )
 }
 
